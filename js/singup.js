e33cdc44e1befc0d3125e54a0e83da80e383f083
Szymon Malinowski <myshon323@gmail.com>
2018-08-20 23:08:52 +0200

    Added vue js

---
+++ js/singup.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#signup',
+    data: {
+        message: 'Hello Vue.js!',
+        opened: true,
+        email: "",
+        firstname: "",
+        lastname: "",
+        people: 1,
+        hotel_all: false,
+        house_all: false,
+        friday: 
+        {
+            breakfast: false,
+            lunch: false,
+            supper: false,
+            house_night: false,
+            hotel_night: false,
+            meal_type: "",
+        }
+    }
+})
